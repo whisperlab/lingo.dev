a5bd78c3c30452a7feaf3b823c5eb55ddd6542d1
maxprilutskiy <maks.prilutskiy@gmail.com>
2025-04-08 10:03:58 -0700

    chore: upd observability params

--- packages/cli/src/cli/utils/observability.ts
+++ packages/cli/src/cli/utils/observability.ts
@@ -7,6 +7,8 @@ export default async function trackEvent(distinctId: string, event: string, prop
 
   const posthog = new PostHog("phc_eR0iSoQufBxNY36k0f0T15UvHJdTfHlh8rJcxsfhfXk", {
     host: "https://eu.i.posthog.com",
+    flushAt: 1,
+    flushInterval: 0,
   });
 
   await posthog.capture({
@@ -14,4 +16,6 @@ export default async function trackEvent(distinctId: string, event: string, prop
     event,
     properties,
   });
+
+  await posthog.shutdown();
 }
