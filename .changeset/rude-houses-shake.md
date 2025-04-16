e8ea95551c8a3b16afe078554ebcb1d79ce817cf
maxprilutskiy <maks.prilutskiy@gmail.com>
2025-04-16 11:15:56 -0700

    fix: gracefully exit on o11y errors

---
+++ .changeset/rude-houses-shake.md
@@ -0,0 +1,5 @@
+---
+"lingo.dev": patch
+---
+
+gracefully exist on o11y errors
--- packages/cli/src/cli/utils/observability.ts
+++ packages/cli/src/cli/utils/observability.ts
@@ -5,23 +5,29 @@ export default async function trackEvent(distinctId: string, event: string, prop
     return;
   }
 
-  const posthog = new PostHog("phc_eR0iSoQufBxNY36k0f0T15UvHJdTfHlh8rJcxsfhfXk", {
-    host: "https://eu.i.posthog.com",
-    flushAt: 1,
-    flushInterval: 0,
-  });
+  try {
+    const posthog = new PostHog("phc_eR0iSoQufBxNY36k0f0T15UvHJdTfHlh8rJcxsfhfXk", {
+      host: "https://eu.i.posthog.com",
+      flushAt: 1,
+      flushInterval: 0,
+    });
 
-  await posthog.capture({
-    distinctId,
-    event,
-    properties: {
-      ...properties,
-      meta: {
-        version: process.env.npm_package_version,
-        isCi: process.env.CI === "true",
+    await posthog.capture({
+      distinctId,
+      event,
+      properties: {
+        ...properties,
+        meta: {
+          version: process.env.npm_package_version,
+          isCi: process.env.CI === "true",
+        },
       },
-    },
-  });
+    });
 
-  await posthog.shutdown();
+    await posthog.shutdown();
+  } catch (error) {
+    if (process.env.DEBUG) {
+      console.error(error);
+    }
+  }
 }
